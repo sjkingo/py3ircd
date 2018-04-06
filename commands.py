d63ca65b9531609b3872cf1b8035c86628472419
Sam Kingston <sam@sjkwi.com.au>
2018-04-07 00:20:06 +1000

    Use ident class

--- commands.py
+++ commands.py
@@ -12,8 +12,8 @@ def NICK(client, nick, *ignore):
     NICK <nickname> [ <hopcount> ]
     https://tools.ietf.org/html/rfc1459#section-4.1.2
     """
-    client.nick = nick
-    if client.registered:
+    client.ident.nick = nick
+    if client.ident.registered:
         client.send_notices()
 
 def USER(client, username, ignore1, ignore2, *realname):
@@ -21,7 +21,7 @@ def USER(client, username, ignore1, ignore2, *realname):
     USER <username> <hostname> <servername> :<realname>
     https://tools.ietf.org/html/rfc1459#section-4.1.3
     """
-    client.username = username
-    client.realname = ' '.join(realname)[1:]
-    if client.registered:
+    client.ident.username = username
+    client.ident.realname = ' '.join(realname)[1:]
+    if client.ident.registered:
         client.send_notices()
--- irc.py
+++ irc.py
@@ -10,15 +10,24 @@ import commands
 
 TERMINATOR = '\r\n'
 
+class Ident:
+    nick = None
+    username = None
+    realname = None
+
+    def __str__(self):
+        return f'{self.username}/{self.nick} ({self.realname})'
+
+    @property
+    def registered(self):
+        return self.nick and self.username and self.realname
+
 class Client:
     """
     Representation of a connected IRC client.
     """
 
-    nick = None
-    username = None
-    realname = None
-
+    ident = Ident()
     code = 1
 
     def __init__(self, transport):
@@ -32,7 +41,7 @@ class Client:
     def peername(self):
         """ip:port<nick>"""
         ip, port = self.transport.get_extra_info('peername')
-        nick = '<{}>'.format(self.nick if self.nick else '(unset)')
+        nick = '<{}>'.format(self.ident.nick if self.ident.nick else '(unset)')
         return '{}:{}{}'.format(ip, port, nick)
 
     def _write(self, line):
@@ -50,7 +59,7 @@ class Client:
         msg = ':{server} {code:03} {nick} :{line}'.format(
                 server=Server.server_name,
                 code=code if code is not None else self.code,
-                nick=self.nick,
+                nick=self.ident.nick,
                 line=line)
         self._write(msg)
         self.code += 1
@@ -79,12 +88,8 @@ class Client:
             else:
                 raise
 
-    @property
-    def registered(self):
-        return self.nick and self.username and self.realname
-
     def send_notices(self):
-        log.debug('C {} {} ({}) is now registered'.format(self, self.username, self.realname))
+        log.debug('C {} {} is now registered'.format(self, self.ident))
         self.send('hello')
 
 class Server:
