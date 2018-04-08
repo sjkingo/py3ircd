f15445f09b9fce2a761186955a7cf3db1388d2fb
Sam Kingston <sam@sjkwi.com.au>
2018-04-08 18:47:55 +1000

    Fix up test script

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./main.py &
+py3ircd/main.py &
 server=$!
 sleep 1
 
-echo -n -e 'CAP LS\r\nNICK test1\r\nUSER test1 test1 localhost :Test 1\r\nPING wren.sjkwi.com.au\r\nJOIN #foo\r\n' | nc localhost 6667 >/dev/null &
+echo -n -e 'NICK test1\r\nUSER test1 test1 localhost :Test 1\r\nJOIN #foo\r\n' | nc localhost 6667 >/dev/null &
 
 sleep 1
 pkill nc
